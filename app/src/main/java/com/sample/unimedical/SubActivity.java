8ebc6465e632b60614ee6bd18102b1e38beabb93
choi <ghchoi0427@gmail.com>
2021-06-30 17:31:36 +0900

    feat: 커뮤니티 액티비티 인텐트 추가

--- app/src/main/java/com/sample/unimedical/SubActivity.java
+++ app/src/main/java/com/sample/unimedical/SubActivity.java
@@ -2,6 +2,7 @@ package com.sample.unimedical;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,9 +15,14 @@ public class SubActivity extends AppCompatActivity {
         setContentView(R.layout.activity_sub);
 
         ImageButton btnSetting = findViewById(R.id.btn_setting);
+        Button btnSearch = findViewById(R.id.btn_search);
 
         btnSetting.setOnClickListener(v -> {
             startActivity(new Intent(SubActivity.this, SettingActivity.class));
+        });
+
+        btnSearch.setOnClickListener(v -> {
+            startActivity(new Intent(SubActivity.this, SearchActivity.class));
         });
     }
 }
