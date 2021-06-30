7a8a32114c4a7d6ea548bafdcb78d5710f495f1d
choi <ghchoi0427@gmail.com>
2021-06-30 17:00:08 +0900

    feat: GlobalApplication 생성

---
+++ app/src/main/java/com/sample/unimedical/GlobalApplication.java
@@ -0,0 +1,17 @@
+package com.sample.unimedical;
+
+import android.app.Application;
+
+import com.kakao.sdk.common.KakaoSdk;
+
+public class GlobalApplication extends Application {
+    private static GlobalApplication instance;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        instance = this;
+        // 네이티브 앱 키로 초기화
+        KakaoSdk.init(this, "c51d81b28660758b6aeee33882fb0881");
+    }
+}
