a5b86d70a67220f3aefd0654b1e61817c371f980
choi <ghchoi0427@gmail.com>
2021-07-01 13:45:20 +0900

    fix: RequestAPI 클래스 삭제

---
+++ app/src/main/java/com/sample/unimedical/RequestAPI.java
@@ -0,0 +1,12 @@
+package com.sample.unimedical;
+
+import android.os.AsyncTask;
+
+import java.net.URL;
+
+public class RequestAPI extends AsyncTask<URL,Integer,Long> {
+    @Override
+    protected Long doInBackground(URL... urls) {
+        return null;
+    }
+}
