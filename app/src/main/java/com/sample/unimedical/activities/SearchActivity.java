ce756bbc0cd76971cd5aa1d2f5e1a9e4e4a2b3db
choi <ghchoi0427@gmail.com>
2021-07-01 11:16:16 +0900

    fix: Thread 생성해서 처리 network on main thread exception: 메인쓰레드에서 http request 불가

--- app/src/main/java/com/sample/unimedical/activities/SearchActivity.java
+++ app/src/main/java/com/sample/unimedical/activities/SearchActivity.java
@@ -1,7 +1,7 @@
 package com.sample.unimedical.activities;
 
 import android.os.Bundle;
-import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -9,11 +9,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.sample.unimedical.R;
 
-import org.xmlpull.v1.XmlPullParser;
-import org.xmlpull.v1.XmlPullParserFactory;
-
 import java.io.BufferedReader;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
@@ -21,11 +17,8 @@ import java.net.URLEncoder;
 
 public class SearchActivity extends AppCompatActivity {
     EditText edit;
-    TextView text;
-    XmlPullParser xpp;
-
-    String key = "자신의 서비스키";
-    String data;
+    TextView result;
+    Button search;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,143 +26,26 @@ public class SearchActivity extends AppCompatActivity {
         setContentView(R.layout.activity_search);
 
         edit = findViewById(R.id.edit);
-        text = findViewById(R.id.result);
+        result = findViewById(R.id.result);
+        search = findViewById(R.id.btn_search_result);
 
-    }
 
-    public void mOnClick(View v) {
-        switch (v.getId()) {
-            case R.id.button:
-                new Thread(new Runnable() {
-                    @Override
-                    public void run() {
-                        data = getXmlData();
-
-                        runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
-                                text.setText(data);
-                            }
-                        });
-                    }
-                }).start();
-                break;
-        }
-    }
-
-    String getXmlData() {
-        StringBuffer buffer = new StringBuffer();
-        String str = edit.getText().toString();//EditText에 작성된 Text얻어오기
-        String location = URLEncoder.encode(str);
-        String query = "%EC%A0%84%EB%A0%A5%EB%A1%9C";
-
-        String queryUrl = "http://openapi.kepco.co.kr/service/EvInfoServiceV2/getEvSearchList?addr=" + location + "&pageNo=1&numOfRows=10&ServiceKey=자신의 서비스키";
-        try {
-            URL url = new URL(queryUrl);//문자열로 된 요청 url을 URL 객체로 생성.
-            InputStream is = url.openStream(); //url위치로 입력스트림 연결
-
-            XmlPullParserFactory factory = XmlPullParserFactory.newInstance();//xml파싱을 위한
-            XmlPullParser xpp = factory.newPullParser();
-            xpp.setInput(new InputStreamReader(is, "UTF-8")); //inputstream 으로부터 xml 입력받기
-
-            String tag;
-
-            xpp.next();
-            int eventType = xpp.getEventType();
-            while (eventType != XmlPullParser.END_DOCUMENT) {
-                switch (eventType) {
-                    case XmlPullParser.START_DOCUMENT:
-                        buffer.append("파싱 시작...\n\n");
-                        break;
-
-                    case XmlPullParser.START_TAG:
-                        tag = xpp.getName();//테그 이름 얻어오기
-
-                        if (tag.equals("item")) ;// 첫번째 검색결과
-                        else if (tag.equals("addr")) {
-                            buffer.append("주소 : ");
-                            xpp.next();
-                            buffer.append(xpp.getText());//title 요소의 TEXT 읽어와서 문자열버퍼에 추가
-                            buffer.append("\n"); //줄바꿈 문자 추가
-                        } else if (tag.equals("chargeTp")) {
-                            buffer.append("충전소타입 : ");
-                            xpp.next();
-                            buffer.append(xpp.getText());//category 요소의 TEXT 읽어와서 문자열버퍼에 추가
-                            buffer.append("\n");//줄바꿈 문자 추가
-                        } else if (tag.equals("cpId")) {
-                            buffer.append("충전소ID :");
-                            xpp.next();
-                            buffer.append(xpp.getText());//description 요소의 TEXT 읽어와서 문자열버퍼에 추가
-                            buffer.append("\n");//줄바꿈 문자 추가
-                        } else if (tag.equals("cpNm")) {
-                            buffer.append("충전기 명칭 :");
-                            xpp.next();
-                            buffer.append(xpp.getText());//telephone 요소의 TEXT 읽어와서 문자열버퍼에 추가
-                            buffer.append("\n");//줄바꿈 문자 추가
-                        } else if (tag.equals("cpStat")) {
-                            buffer.append("충전기 상태 코드 :");
-                            xpp.next();
-                            buffer.append(xpp.getText());//address 요소의 TEXT 읽어와서 문자열버퍼에 추가
-                            buffer.append("\n");//줄바꿈 문자 추가
-                        } else if (tag.equals("cpTp")) {
-                            buffer.append("충전 방식 :");
-                            xpp.next();
-                            buffer.append(xpp.getText());//mapx 요소의 TEXT 읽어와서 문자열버퍼에 추가
-                            buffer.append("  ,  "); //줄바꿈 문자 추가
-                        } else if (tag.equals("csId")) {
-                            buffer.append("충전소 ID :");
-                            xpp.next();
-                            buffer.append(xpp.getText());//mapy 요소의 TEXT 읽어와서 문자열버퍼에 추가
-                            buffer.append("\n"); //줄바꿈 문자 추가
-                        } else if (tag.equals("cpNm")) {
-                            buffer.append("충전소 명칭 :");
-                            xpp.next();
-                            buffer.append(xpp.getText());//mapy 요소의 TEXT 읽어와서 문자열버퍼에 추가
-                            buffer.append("\n"); //줄바꿈 문자 추가
-                        } else if (tag.equals("lat")) {
-                            buffer.append("위도 :");
-                            xpp.next();
-                            buffer.append(xpp.getText());//mapy 요소의 TEXT 읽어와서 문자열버퍼에 추가
-                            buffer.append("\n"); //줄바꿈 문자 추가
-                        } else if (tag.equals("longi")) {
-                            buffer.append("경도 :");
-                            xpp.next();
-                            buffer.append(xpp.getText());//mapy 요소의 TEXT 읽어와서 문자열버퍼에 추가
-                            buffer.append("\n"); //줄바꿈 문자 추가
-                        } else if (tag.equals("statUpdateDatetime")) {
-                            buffer.append("충전기상태갱신시각 :");
-                            xpp.next();
-                            buffer.append(xpp.getText());//mapy 요소의 TEXT 읽어와서 문자열버퍼에 추가
-                            buffer.append("\n"); //줄바꿈 문자 추가
-                        }
-                        break;
-
-                    case XmlPullParser.TEXT:
-                        break;
-
-                    case XmlPullParser.END_TAG:
-                        tag = xpp.getName(); //테그 이름 얻어오기
-
-                        if (tag.equals("item")) buffer.append("\n");// 첫번째 검색결과종료..줄바꿈
-                        break;
+        search.setOnClickListener(v -> {
+            new Thread(() -> {
+                try {
+                    request();
+                } catch (Exception e) {
+                    e.printStackTrace();
                 }
+            }).start();
+        });
+    }
 
-                eventType = xpp.next();
-            }
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        buffer.append("파싱 끝\n");
-        return buffer.toString();//StringBuffer 문자열 객체 반환
-
-    }//getXmlData method....
 
     private void request() throws Exception {
         StringBuilder urlBuilder = new StringBuilder("http://apis.data.go.kr/1471000/MdeqPrdlstInfoService/getMdeqPrdlstInfoInq"); /*URL*/
         urlBuilder.append("?" + URLEncoder.encode("ServiceKey", "UTF-8") + "=서비스키"); /*Service Key*/
-        urlBuilder.append("&" + URLEncoder.encode("ServiceKey", "UTF-8") + "=" + URLEncoder.encode("인증키 (URL Encode)", "UTF-8")); /*공공데이터포털에서 받은 인증키*/
+        urlBuilder.append("&" + URLEncoder.encode("ServiceKey", "UTF-8") + "=" + URLEncoder.encode("ZahPi3DSCug%2Bb6Q%2FDPcRwDg0AsTPAyIfVRdozdEXUw3UTLcCr3oEt6Ml3iOSwiXs2Rx2kT9giR6RuLeaPGzLRg%3D%3D", "UTF-8")); /*공공데이터포털에서 받은 인증키*/
         urlBuilder.append("&" + URLEncoder.encode("pageNo", "UTF-8") + "=" + URLEncoder.encode("1", "UTF-8")); /*페이지번호*/
         urlBuilder.append("&" + URLEncoder.encode("numOfRows", "UTF-8") + "=" + URLEncoder.encode("3", "UTF-8")); /*한 페이지 결과 수*/
         urlBuilder.append("&" + URLEncoder.encode("PRDLST_NM", "UTF-8") + "=" + URLEncoder.encode("의료용침대", "UTF-8")); /*품목명*/
@@ -178,23 +54,20 @@ public class SearchActivity extends AppCompatActivity {
         HttpURLConnection conn = (HttpURLConnection) url.openConnection();
         conn.setRequestMethod("GET");
         conn.setRequestProperty("Content-type", "application/json");
-        System.out.println("Response code: " + conn.getResponseCode());
+        //System.out.println("Response code: " + conn.getResponseCode());
         BufferedReader rd;
         if (conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
             rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
         } else {
             rd = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
         }
-        StringBuilder sb = new StringBuilder();
         String line;
         while ((line = rd.readLine()) != null) {
-            sb.append(line);
+            result.append(line);
         }
         rd.close();
         conn.disconnect();
-        System.out.println(sb.toString());
     }
-
 
 }
 
