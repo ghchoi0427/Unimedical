7f8309aa3518538e7b2d094046c3318565342e38
choi <ghchoi0427@gmail.com>
2021-07-01 13:46:54 +0900

    fix: Request 양식 수정

--- app/src/main/java/com/sample/unimedical/activities/SearchActivity.java
+++ app/src/main/java/com/sample/unimedical/activities/SearchActivity.java
@@ -41,20 +41,17 @@ public class SearchActivity extends AppCompatActivity {
         });
     }
 
-
     private void request() throws Exception {
         StringBuilder urlBuilder = new StringBuilder("http://apis.data.go.kr/1471000/MdeqPrdlstInfoService/getMdeqPrdlstInfoInq"); /*URL*/
-        urlBuilder.append("?" + URLEncoder.encode("ServiceKey", "UTF-8") + "=서비스키"); /*Service Key*/
-        urlBuilder.append("&" + URLEncoder.encode("ServiceKey", "UTF-8") + "=" + URLEncoder.encode("ZahPi3DSCug%2Bb6Q%2FDPcRwDg0AsTPAyIfVRdozdEXUw3UTLcCr3oEt6Ml3iOSwiXs2Rx2kT9giR6RuLeaPGzLRg%3D%3D", "UTF-8")); /*공공데이터포털에서 받은 인증키*/
+        urlBuilder.append("?" + URLEncoder.encode("serviceKey", "UTF-8") + "=" + URLEncoder.encode("uhPZ+yjcUrJD5qN1Q6Wf1+o63BmTtVFSTTKYCRPT0JY7HN934bPpj4S5f2QQng+LHjCADIGxjrHTUE0pGXJfGA==", "UTF-8")); /*공공데이터포털에서 받은 인증키*/
         urlBuilder.append("&" + URLEncoder.encode("pageNo", "UTF-8") + "=" + URLEncoder.encode("1", "UTF-8")); /*페이지번호*/
         urlBuilder.append("&" + URLEncoder.encode("numOfRows", "UTF-8") + "=" + URLEncoder.encode("3", "UTF-8")); /*한 페이지 결과 수*/
         urlBuilder.append("&" + URLEncoder.encode("PRDLST_NM", "UTF-8") + "=" + URLEncoder.encode("의료용침대", "UTF-8")); /*품목명*/
-        urlBuilder.append("&" + URLEncoder.encode("type", "UTF-8") + "=" + URLEncoder.encode("xml", "UTF-8")); /*응답데이터 형식(xml/json) Default: xml*/
+        urlBuilder.append("&" + URLEncoder.encode("type", "UTF-8") + "=" + URLEncoder.encode("json", "UTF-8")); /*응답데이터 형식(xml/json) Default: xml*/
         URL url = new URL(urlBuilder.toString());
         HttpURLConnection conn = (HttpURLConnection) url.openConnection();
         conn.setRequestMethod("GET");
         conn.setRequestProperty("Content-type", "application/json");
-        //System.out.println("Response code: " + conn.getResponseCode());
         BufferedReader rd;
         if (conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
             rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
@@ -62,9 +59,11 @@ public class SearchActivity extends AppCompatActivity {
             rd = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
         }
         String line;
+        StringBuilder sb = new StringBuilder();
         while ((line = rd.readLine()) != null) {
-            result.append(line);
+            sb.append(line);
         }
+        result.append(sb.toString());
         rd.close();
         conn.disconnect();
     }
