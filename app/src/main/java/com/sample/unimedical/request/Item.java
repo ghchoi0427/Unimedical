f727cd2761638149e42e2e120a19a6aa9000d287
choi <ghchoi0427@gmail.com>
2021-07-02 13:24:03 +0900

    fix: getter 생성, rename

---
+++ app/src/main/java/com/sample/unimedical/request/Item.java
@@ -0,0 +1,95 @@
+package com.sample.unimedical.request;
+
+public class Item {
+    private String MDEQ_PRDLST_SN;
+    private String SIZE_INFO;
+    private String CHRTR_INFO;
+    private String MNFT_MTH_CONT;
+    private String USE_PURPS_CONT;
+    private String USE_MTH_CONT;
+    private String ATTN_MTTR_CONT;
+    private String VLD_PRD_INFO;
+    private String TEST_STND_CONT;
+    private String INST_AREA_DIVS_NM;
+    private String INDT_NM;
+    private String PRMSN_DCLR_DIVS_NM;
+    private String MEDDEV_ITEM_NO;
+    private String PRDLST_NM;
+    private String PRMSN_YMD;
+    private String RTRCN_DSCTN_DIVS_CD;
+    private String RTRCN_DSCTN_DT;
+    private String PRDT_NM_INFO;
+
+    public String getMDEQ_PRDLST_SN() {
+        return MDEQ_PRDLST_SN;
+    }
+
+    public String getSIZE_INFO() {
+        return SIZE_INFO;
+    }
+
+    public String getCHRTR_INFO() {
+        return CHRTR_INFO;
+    }
+
+    public String getMNFT_MTH_CONT() {
+        return MNFT_MTH_CONT;
+    }
+
+    public String getUSE_PURPS_CONT() {
+        return USE_PURPS_CONT;
+    }
+
+    public String getUSE_MTH_CONT() {
+        return USE_MTH_CONT;
+    }
+
+    public String getATTN_MTTR_CONT() {
+        return ATTN_MTTR_CONT;
+    }
+
+    public String getVLD_PRD_INFO() {
+        return VLD_PRD_INFO;
+    }
+
+    public String getTEST_STND_CONT() {
+        return TEST_STND_CONT;
+    }
+
+    public String getINST_AREA_DIVS_NM() {
+        return INST_AREA_DIVS_NM;
+    }
+
+    public String getINDT_NM() {
+        return INDT_NM;
+    }
+
+    public String getPRMSN_DCLR_DIVS_NM() {
+        return PRMSN_DCLR_DIVS_NM;
+    }
+
+    public String getMEDDEV_ITEM_NO() {
+        return MEDDEV_ITEM_NO;
+    }
+
+    public String getPRDLST_NM() {
+        return PRDLST_NM;
+    }
+
+    public String getPRMSN_YMD() {
+        return PRMSN_YMD;
+    }
+
+    public String getRTRCN_DSCTN_DIVS_CD() {
+        return RTRCN_DSCTN_DIVS_CD;
+    }
+
+    public String getRTRCN_DSCTN_DT() {
+        return RTRCN_DSCTN_DT;
+    }
+
+    public String getPRDT_NM_INFO() {
+        return PRDT_NM_INFO;
+    }
+
+}
