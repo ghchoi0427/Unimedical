4e76738455896f4200f654140e9d742c55d4d104
choi <ghchoi0427@gmail.com>
2021-07-02 15:16:16 +0900

    feat: 어댑터 생성

---
+++ app/src/main/java/com/sample/unimedical/activities/DeviceAdapter.java
@@ -0,0 +1,70 @@
+package com.sample.unimedical.activities;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.sample.unimedical.R;
+import com.sample.unimedical.device.Item;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+
+public class DeviceAdapter extends RecyclerView.Adapter<DeviceAdapter.ViewHolder> {
+    ArrayList<Item> items = new ArrayList<Item>();
+
+    @NonNull
+    @NotNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull @NotNull ViewGroup viewGroup, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(viewGroup.getContext());
+        View itemView = inflater.inflate(R.layout.device_item, viewGroup, false);
+
+        return new ViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull @NotNull ViewHolder viewHolder, int position) {
+        Item item = items.get(position);
+        viewHolder.setItem(item);
+    }
+
+    @Override
+    public int getItemCount() {
+        return items.size();
+    }
+
+    public void addItem(Item item) {
+        items.add(item);
+    }
+
+    public void setItems(ArrayList<Item> items) {
+        this.items = items;
+    }
+
+    public Item getItem(int position) {
+        return items.get(position);
+    }
+
+    static class ViewHolder extends RecyclerView.ViewHolder {
+        TextView textView;
+        TextView textView2;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+
+            textView = itemView.findViewById(R.id.textItemName);
+            textView = itemView.findViewById(R.id.textItemPurpose);
+        }
+
+        public void setItem(Item item) {
+            textView.setText(item.getPRDLST_NM());
+            textView2.setText(item.getUSE_PURPS_CONT());
+        }
+    }
+}
