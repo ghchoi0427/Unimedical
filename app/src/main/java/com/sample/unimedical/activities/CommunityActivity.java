37e0e4fceca3e68900bb05feabf3844640e78505
choi <ghchoi0427@gmail.com>
2021-07-02 14:15:59 +0900

    feat: 웹 뷰 구현

--- app/src/main/java/com/sample/unimedical/activities/CommunityActivity.java
+++ app/src/main/java/com/sample/unimedical/activities/CommunityActivity.java
@@ -1,16 +1,34 @@
 package com.sample.unimedical.activities;
 
 import android.os.Bundle;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.sample.unimedical.R;
 
 public class CommunityActivity extends AppCompatActivity {
+    WebView webView;
+
+    private String url = "http://www.medicaltimes.com/Users/index.html?_REFERER=";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_community);
+
+        webView = findViewById(R.id.webview);
+        try{
+
+            webView.getSettings().setJavaScriptEnabled(true);
+            webView.loadUrl(url);
+            webView.setWebChromeClient(new WebChromeClient());
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+
     }
 }
