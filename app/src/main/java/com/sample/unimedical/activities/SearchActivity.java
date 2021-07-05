45626be4dbfeaf807224c0c6979e5760067d7b16
choi <ghchoi0427@gmail.com>
2021-07-05 13:14:31 +0900

    refactor: 필요없는 import 삭제

--- app/src/main/java/com/sample/unimedical/activities/SearchActivity.java
+++ app/src/main/java/com/sample/unimedical/activities/SearchActivity.java
@@ -1,11 +1,9 @@
 package com.sample.unimedical.activities;
 
-import android.bluetooth.BluetoothClass;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
