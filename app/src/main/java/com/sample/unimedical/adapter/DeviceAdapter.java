462c42c89e39bb3c0cfcad68766c070491f38d04
choi <ghchoi0427@gmail.com>
2021-07-05 16:20:28 +0900

    feat: 품목 번호, 기관명 추가

--- app/src/main/java/com/sample/unimedical/adapter/DeviceAdapter.java
+++ app/src/main/java/com/sample/unimedical/adapter/DeviceAdapter.java
@@ -1,5 +1,6 @@
 package com.sample.unimedical.adapter;
 
+import android.annotation.SuppressLint;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,6 +17,8 @@ import java.util.ArrayList;
 public class DeviceAdapter extends RecyclerView.Adapter<DeviceAdapter.ViewHolder> {
     ArrayList<Item> items = new ArrayList<>();
     static final String textSerialNumber = "시리얼번호 : ";
+    static final String textInstitution = "기관지역구분 : ";
+    static final String textItemNumber = "의료기기품목허가번호 : ";
 
     public ArrayList<Item> getItems() {
         return items;
@@ -61,6 +64,8 @@ public class DeviceAdapter extends RecyclerView.Adapter<DeviceAdapter.ViewHolder
         TextView itemName;
         TextView itemPurpose;
         TextView serialNumber;
+        TextView institution;
+        TextView itemNumber;
 
         public ViewHolder(View itemView) {
             super(itemView);
@@ -68,12 +73,17 @@ public class DeviceAdapter extends RecyclerView.Adapter<DeviceAdapter.ViewHolder
             itemName = itemView.findViewById(R.id.text_item_name);
             itemPurpose = itemView.findViewById(R.id.text_item_purpose);
             serialNumber = itemView.findViewById(R.id.text_serial_number);
+            institution = itemView.findViewById(R.id.text_inst_div);
+            itemNumber = itemView.findViewById(R.id.text_item_number);
         }
 
+        @SuppressLint("SetTextI18n")
         public void setItem(Item item) {
             itemName.setText(item.getPRDLST_NM());
             itemPurpose.setText(item.getUSE_PURPS_CONT());
-            serialNumber.setText(textSerialNumber+item.getMDEQ_PRDLST_SN());
+            serialNumber.setText(textSerialNumber + item.getMDEQ_PRDLST_SN());
+            serialNumber.setText(textInstitution + item.getINST_AREA_DIVS_NM());
+            itemNumber.setText(textItemNumber + item.getMEDDEV_ITEM_NO());
         }
     }
 }
