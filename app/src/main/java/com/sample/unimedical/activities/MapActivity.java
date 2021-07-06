302677eb71c65fb9a21e8ca34daa70d62d16ea10
choi <ghchoi0427@gmail.com>
2021-07-06 16:34:41 +0900

    feat: map activity 추가

---
+++ app/src/main/java/com/sample/unimedical/activities/MapActivity.java
@@ -0,0 +1,16 @@
+package com.sample.unimedical.activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.sample.unimedical.R;
+
+public class MapActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+    }
+}
