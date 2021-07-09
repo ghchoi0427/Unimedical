449bada19b236b9b79dbca3829f4747d80044e9b
choi <ghchoi0427@gmail.com>
2021-07-09 09:45:40 +0900

    fix: 데이터 변경(더 가공된 엑셀 데이터)

--- app/src/main/java/com/sample/unimedical/activities/SearchActivity.java
+++ app/src/main/java/com/sample/unimedical/activities/SearchActivity.java
@@ -1,11 +1,13 @@
 package com.sample.unimedical.activities;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.TextView;
 
 import androidx.annotation.IdRes;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,152 +21,135 @@ import com.sample.unimedical.device.Item;
 import com.sample.unimedical.device.Response;
 
 import java.io.BufferedReader;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+import jxl.Cell;
+import jxl.Sheet;
+import jxl.Workbook;
 
 public class SearchActivity extends AppCompatActivity {
-    EditText edit;
-    Button search;
-    Button previous;
-    Button next;
-    RecyclerView recyclerView;
-    DeviceAdapter adapter;
-    RadioButton loadTen;
-    RadioButton loadFifty;
-    RadioButton loadHundred;
-    RadioGroup radioGroup;
-
-    private int pageNumber = 1;
-    private int numberOfRows = 30;
-    private final int firstPage = 1;
+    EditText editText;
+    Button searchButton;
+    TextView resultView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
 
-        edit = findViewById(R.id.edit);
-        search = findViewById(R.id.btn_search_result);
-        recyclerView = findViewById(R.id.recyclerView);
-        adapter = new DeviceAdapter();
-        previous = findViewById(R.id.btn_previous);
-        next = findViewById(R.id.btn_next);
+        editText = findViewById(R.id.edit);
+        searchButton = findViewById(R.id.btn_search_result);
+        resultView = findViewById(R.id.imptextview);
 
-        previous.setVisibility(View.INVISIBLE);
-        next.setVisibility(View.INVISIBLE);
+        // readExcel();
 
-        radioGroup = findViewById(R.id.radio_group);
-        loadTen = findViewById(R.id.load_ten);
-        loadFifty = findViewById(R.id.load_fifty);
-        loadHundred = findViewById(R.id.load_hundred);
 
-        LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
-        recyclerView.setLayoutManager(layoutManager);
-        recyclerView.setAdapter(adapter);
-
-        search.setOnClickListener(v -> new Thread(() -> {
-            runOnUiThread(() -> previous.setVisibility(View.VISIBLE));
-            runOnUiThread(() -> next.setVisibility(View.VISIBLE));
-
-            try {
-                startSearch();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }).start());
-
-        previous.setOnClickListener(v -> new Thread(() -> {
-            if (pageNumber > firstPage) {
-                pageNumber--;
-                try {
-                    startSearch();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        }).start());
-
-        next.setOnClickListener(v -> new Thread(() -> {
-            pageNumber++;
-            try {
-                startSearch();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }).start());
-
-        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(RadioGroup group, int checkedId) {
-                if (checkedId == R.id.load_ten) {
-                    numberOfRows = 10;
-                }
-                if (checkedId == R.id.load_fifty) {
-                    numberOfRows = 50;
-                }
-
-                if (checkedId == R.id.load_hundred) {
-                    numberOfRows = 100;
-                }
-            }
+        searchButton.setOnClickListener(v -> {
+            readExcel(editText.getText().toString());
         });
 
 
     }
 
-    private void startSearch() throws Exception {
-        processResponse(request());
-    }
 
-    public void processResponse(String response) {
-        Gson gson = new Gson();
-        Response fromJson = gson.fromJson(response, Response.class);
+    private void readExcel() {
+        try {
+            InputStream is = getBaseContext().getResources().getAssets().open("device_data.xls");
 
-        clearItemList(adapter);
+            Workbook wb = Workbook.getWorkbook(is);
 
-        for (Item i : fromJson.getBody().getItems()) {
-            adapter.addItem(i);
+            if (wb != null) {
+                Sheet sheet = wb.getSheet(0);
+                if (sheet != null) {
+                    int colTotal = sheet.getColumns();
+                    int rowIndexStart = 1;
+                    int rowTotal = sheet.getColumn(colTotal - 1).length;
+
+                    StringBuilder sb;
+
+                    for (int row = rowIndexStart; row < rowTotal; row++) {
+                        sb = new StringBuilder();
+
+                        for (int col = 0; col < colTotal; col++) {
+                            String contents = sheet.getCell(col, row).getContents();
+                            Log.d("test", col + "번째: " + contents);
+                        }
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-
-        notifyDataSetChanged(adapter);
     }
 
-    private void clearItemList(DeviceAdapter adapter) {
-        adapter.clearItem();
-    }
+    private void readExcel(String keyword) {
+        try {
+            print("keyword= " + keyword);
+            print("flag1");
+            InputStream is = getBaseContext().getResources().getAssets().open("meddev_list.xls");
+            print("flag2");
 
-    private void notifyDataSetChanged(DeviceAdapter adapter) {
-        runOnUiThread(adapter::notifyDataSetChanged);
-    }
+            Workbook wb = Workbook.getWorkbook(is);
+            print("flag3");
 
-    private String request() throws Exception {
-        String urlBuilder = "http://apis.data.go.kr/1471000/MdeqPrdlstInfoService/getMdeqPrdlstInfoInq" + "?" + URLEncoder.encode("serviceKey", "UTF-8") + "=" + URLEncoder.encode("uhPZ+yjcUrJD5qN1Q6Wf1+o63BmTtVFSTTKYCRPT0JY7HN934bPpj4S5f2QQng+LHjCADIGxjrHTUE0pGXJfGA==", "UTF-8") + /*공공데이터포털에서 받은 인증키*/
-                "&" + URLEncoder.encode("pageNo", "UTF-8") + "=" + URLEncoder.encode(String.valueOf(pageNumber), "UTF-8") + /*페이지번호*/
-                "&" + URLEncoder.encode("numOfRows", "UTF-8") + "=" + URLEncoder.encode(String.valueOf(numberOfRows), "UTF-8") + /*한 페이지 결과 수*/
-                "&" + URLEncoder.encode("PRDLST_NM", "UTF-8") + "=" + URLEncoder.encode(String.valueOf(edit.getText()), "UTF-8") + /*품목명*/
-                "&" + URLEncoder.encode("type", "UTF-8") + "=" + URLEncoder.encode("json", "UTF-8");/*URL*//*응답데이터 형식(xml/json) Default: xml*/
-        URL url = new URL(urlBuilder);
-        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-        conn.setRequestMethod("GET");
-        conn.setRequestProperty("Content-type", "application/json");
-        BufferedReader rd;
-        if (conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
-            rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-        } else {
-            rd = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
+            if (wb != null) {
+                print("flag4");
+                Sheet sheet = wb.getSheet(0);
+                print("flag5");
+                if (sheet != null) {
+                    print("flag6");
+                    int colTotal = sheet.getColumns();
+                    print("flag7");
+                    int rowIndexStart = 1;
+                    print("flag8");
+                    int rowTotal = sheet.getColumn(colTotal - 1).length;
+                    print("flag9");
+
+                    StringBuilder sb = null;
+                    print("flag10");
+
+                    print(rowIndexStart+"");
+                    print(rowTotal+"");
+                    print(colTotal+"");
+
+                    for (int row = rowIndexStart; row < rowTotal; row++) {
+                        print("flag11");
+                        sb = new StringBuilder();
+
+                        for (int col = 0; col < colTotal; col++) {
+                            String contents = sheet.getCell(col, row).getContents();
+
+                            //Log.d("test2", col + " : " + contents);
+                            //print("contents="+contents);
+
+                            if (contents.contains(keyword)) {
+                               for(Cell c :sheet.getRow(row)){
+                                   printTextView(c.getContents());
+                               }
+                            }
+
+                        }
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        String line;
-        StringBuilder sb = new StringBuilder();
-        while ((line = rd.readLine()) != null) {
-            sb.append(line);
-        }
+    }
 
-        rd.close();
-        conn.disconnect();
+    private void printTextView(String s){
+        //resultView.setText("");
+        resultView.append(s);
+    }
 
-        return sb.toString();
+    private void print(String s) {
+        Log.d("test1", s);
     }
 
 }
