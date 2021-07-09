3d0a6125d3443b8ba93c91fd64cda18a80b0d929
choi <ghchoi0427@gmail.com>
2021-07-09 14:18:44 +0900

    feat: item list 생성

---
+++ app/src/main/java/com/sample/unimedical/device/ItemList.java
@@ -0,0 +1,11 @@
+package com.sample.unimedical.device;
+
+import java.util.ArrayList;
+
+public class ItemList {
+    ArrayList<Item> items = new ArrayList<>();
+
+    public ArrayList<Item> getItems() {
+        return items;
+    }
+}
