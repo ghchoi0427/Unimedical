7c8d098d023eae934695f317378526a879d73083
choi <ghchoi0427@gmail.com>
2021-07-09 14:17:24 +0900

    feat: device 정보 변경

--- app/src/main/java/com/sample/unimedical/adapter/DeviceAdapter.java
+++ app/src/main/java/com/sample/unimedical/adapter/DeviceAdapter.java
@@ -16,9 +16,6 @@ import java.util.ArrayList;
 
 public class DeviceAdapter extends RecyclerView.Adapter<DeviceAdapter.ViewHolder> {
     ArrayList<Item> items = new ArrayList<>();
-    static final String textSerialNumber = "시리얼번호 : ";
-    static final String textInstitution = "기관지역구분 : ";
-    static final String textItemNumber = "의료기기품목허가번호 : ";
 
     public ArrayList<Item> getItems() {
         return items;
@@ -62,28 +59,53 @@ public class DeviceAdapter extends RecyclerView.Adapter<DeviceAdapter.ViewHolder
 
     static class ViewHolder extends RecyclerView.ViewHolder {
         TextView itemName;
-        TextView itemPurpose;
-        TextView serialNumber;
-        TextView institution;
-        TextView itemNumber;
+        TextView primaryCode;
+        TextView midClass;
+        TextView midCode;
+        TextView size;
+        TextView unit;
+        TextView maker;
+        TextView material;
+        TextView vendor;
+        TextView priceMax;
+        TextView update;
+        TextView validFrom;
+        TextView remark;
 
         public ViewHolder(View itemView) {
             super(itemView);
 
-            itemName = itemView.findViewById(R.id.text_item_name);
-            itemPurpose = itemView.findViewById(R.id.text_item_purpose);
-            serialNumber = itemView.findViewById(R.id.text_serial_number);
-            institution = itemView.findViewById(R.id.text_inst_div);
-            itemNumber = itemView.findViewById(R.id.text_item_number);
+            itemName = itemView.findViewById(R.id.text_productname);
+            primaryCode = itemView.findViewById(R.id.text_primary);
+            midClass = itemView.findViewById(R.id.text_midclass);
+            midCode = itemView.findViewById(R.id.text_midcode);
+            size = itemView.findViewById(R.id.text_size);
+            unit = itemView.findViewById(R.id.text_unit);
+            maker = itemView.findViewById(R.id.text_maker);
+            material = itemView.findViewById(R.id.text_material);
+            vendor = itemView.findViewById(R.id.text_vendor);
+            priceMax = itemView.findViewById(R.id.text_pricemax);
+            update = itemView.findViewById(R.id.text_update);
+            validFrom = itemView.findViewById(R.id.text_validfrom);
+            remark = itemView.findViewById(R.id.text_remark);
+
         }
 
-        @SuppressLint("SetTextI18n")
         public void setItem(Item item) {
-            itemName.setText(item.getPRDLST_NM());
-            itemPurpose.setText(item.getUSE_PURPS_CONT());
-            serialNumber.setText(textSerialNumber + item.getMDEQ_PRDLST_SN());
-            serialNumber.setText(textInstitution + item.getINST_AREA_DIVS_NM());
-            itemNumber.setText(textItemNumber + item.getMEDDEV_ITEM_NO());
+            itemName.setText(item.getProductName());
+            primaryCode.setText(item.getPrimaryCode());
+            midClass.setText(item.getMidClass());
+            midCode.setText(item.getMidCode());
+            size.setText(item.getSize());
+            unit.setText(item.getUnit());
+            maker.setText(item.getMaker());
+            material.setText(item.getMaterial());
+            vendor.setText(item.getVendor());
+            priceMax.setText(item.getPriceMax());
+            update.setText(item.getUpdate());
+            validFrom.setText(item.getValidFrom());
+            remark.setText(item.getRemark());
+
         }
     }
 }
