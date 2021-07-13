baf677fc97360a07a5431017672269bacdedfa92
choi <ghchoi0427@gmail.com>
2021-07-13 16:46:51 +0900

    refactor: 빈칸 없애기

--- app/src/main/java/com/sample/unimedical/activities/SubActivity.java
+++ app/src/main/java/com/sample/unimedical/activities/SubActivity.java
@@ -2,7 +2,6 @@ package com.sample.unimedical.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 
@@ -13,7 +12,7 @@ import com.sample.unimedical.R;
 public class SubActivity extends AppCompatActivity {
     ImageButton btnSetting;
     Button btnSearchEngine;
-    Button btnCommunity ;
+    Button btnCommunity;
     Button buttonMap;
 
     @Override
