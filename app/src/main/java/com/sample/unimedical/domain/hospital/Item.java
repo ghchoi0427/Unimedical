a1b24cf9c2a38c356332b02f03a5418af66c9dce
choi <ghchoi0427@gmail.com>
2021-07-14 11:09:50 +0900

    feat: setter 추가

--- app/src/main/java/com/sample/unimedical/domain/hospital/Item.java
+++ app/src/main/java/com/sample/unimedical/domain/hospital/Item.java
@@ -26,12 +26,128 @@ public class Item {
     private String sidoCd;
     private String sidoCdNm;
     private String telno;
-    private float XPos;
-    private float YPos;
+    private String XPos;
+    private String YPos;
     private String yadmNm;
     private String ykiho;
     private String addr;
 
+
+    public void setClCd(String clCd) {
+        this.clCd = clCd;
+    }
+
+    public void setClCdNm(String clCdNm) {
+        this.clCdNm = clCdNm;
+    }
+
+    public void setCmdcGdrCnt(String cmdcGdrCnt) {
+        this.cmdcGdrCnt = cmdcGdrCnt;
+    }
+
+    public void setCmdcIntnCnt(String cmdcIntnCnt) {
+        this.cmdcIntnCnt = cmdcIntnCnt;
+    }
+
+    public void setCmdcResdntCnt(String cmdcResdntCnt) {
+        this.cmdcResdntCnt = cmdcResdntCnt;
+    }
+
+    public void setCmdcSdrCnt(String cmdcSdrCnt) {
+        this.cmdcSdrCnt = cmdcSdrCnt;
+    }
+
+    public void setDetyGdrCnt(String detyGdrCnt) {
+        this.detyGdrCnt = detyGdrCnt;
+    }
+
+    public void setDetyIntnCnt(String detyIntnCnt) {
+        this.detyIntnCnt = detyIntnCnt;
+    }
+
+    public void setDetyResdntCnt(String detyResdntCnt) {
+        this.detyResdntCnt = detyResdntCnt;
+    }
+
+    public void setDetySdrCnt(String detySdrCnt) {
+        this.detySdrCnt = detySdrCnt;
+    }
+
+    public void setDistance(String distance) {
+        this.distance = distance;
+    }
+
+    public void setDrTotCnt(String drTotCnt) {
+        this.drTotCnt = drTotCnt;
+    }
+
+    public void setEstbDd(String estbDd) {
+        this.estbDd = estbDd;
+    }
+
+    public void setHospUrl(String hospUrl) {
+        this.hospUrl = hospUrl;
+    }
+
+    public void setMdeptGdrCnt(String mdeptGdrCnt) {
+        this.mdeptGdrCnt = mdeptGdrCnt;
+    }
+
+    public void setMdeptIntnCnt(String mdeptIntnCnt) {
+        this.mdeptIntnCnt = mdeptIntnCnt;
+    }
+
+    public void setMdeptResdntCnt(String mdeptResdntCnt) {
+        this.mdeptResdntCnt = mdeptResdntCnt;
+    }
+
+    public void setMdeptSdrCnt(String mdeptSdrCnt) {
+        this.mdeptSdrCnt = mdeptSdrCnt;
+    }
+
+    public void setPostNo(String postNo) {
+        this.postNo = postNo;
+    }
+
+    public void setSgguCd(String sgguCd) {
+        this.sgguCd = sgguCd;
+    }
+
+    public void setSgguCdNm(String sgguCdNm) {
+        this.sgguCdNm = sgguCdNm;
+    }
+
+    public void setSidoCd(String sidoCd) {
+        this.sidoCd = sidoCd;
+    }
+
+    public void setSidoCdNm(String sidoCdNm) {
+        this.sidoCdNm = sidoCdNm;
+    }
+
+    public void setTelno(String telno) {
+        this.telno = telno;
+    }
+
+    public void setXPos(String XPos) {
+        this.XPos = XPos;
+    }
+
+    public void setYPos(String YPos) {
+        this.YPos = YPos;
+    }
+
+    public void setYadmNm(String yadmNm) {
+        this.yadmNm = yadmNm;
+    }
+
+    public void setYkiho(String ykiho) {
+        this.ykiho = ykiho;
+    }
+
+    public void setAddr(String addr) {
+        this.addr = addr;
+    }
 
     public String getAddr() {
         return addr;
@@ -133,11 +249,11 @@ public class Item {
         return telno;
     }
 
-    public float getXPos() {
+    public String getXPos() {
         return XPos;
     }
 
-    public float getYPos() {
+    public String getYPos() {
         return YPos;
     }
 
