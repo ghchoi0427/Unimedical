19ca9eb7f63a54180ca0b52c31fe1c9345d66908
choi <ghchoi0427@gmail.com>
2021-07-15 10:13:47 +0900

    feat: 이벤트 리스너 설정

--- app/src/main/java/com/sample/unimedical/activities/MapActivity.java
+++ app/src/main/java/com/sample/unimedical/activities/MapActivity.java
@@ -1,16 +1,22 @@
 package com.sample.unimedical.activities;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.RelativeLayout;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.sample.unimedical.R;
 import com.sample.unimedical.domain.hospital.Item;
 
+import net.daum.mf.map.api.CalloutBalloonAdapter;
 import net.daum.mf.map.api.MapPOIItem;
 import net.daum.mf.map.api.MapPoint;
+import net.daum.mf.map.api.MapReverseGeoCoder;
 import net.daum.mf.map.api.MapView;
 
 import org.w3c.dom.Document;
@@ -32,7 +38,7 @@ import java.util.List;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 
-public class MapActivity extends AppCompatActivity implements MapView.CurrentLocationEventListener {
+public class MapActivity extends AppCompatActivity implements MapView.CurrentLocationEventListener, MapView.OpenAPIKeyAuthenticationResultListener, MapView.MapViewEventListener {
 
     private static final int GPS_ENABLE_REQUEST_CODE = 2001;
     private static final int PERMISSIONS_REQUEST_CODE = 100;
@@ -40,25 +46,35 @@ public class MapActivity extends AppCompatActivity implements MapView.CurrentLoc
     RelativeLayout mapViewContainer;
     List<Item> hospitalItems;
 
+    private final String LOG_TAG = "test2";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
 
         mapView = new MapView(this);
+
+        mapView.setDaumMapApiKey("0cfe9165fbf7d7069b488e119b2e8d6c");
+        mapView.setOpenAPIKeyAuthenticationResultListener(this);
+        mapView.setMapViewEventListener(this);
+        mapView.setCurrentLocationEventListener(this);
+
         mapViewContainer = findViewById(R.id.mapView);
         mapViewContainer.addView(mapView);
         hospitalItems = new ArrayList<>();
 
+        currentLocation();
 
-        new Thread(() -> {
+
+        /*new Thread(() -> {
             try {
                 processXML(getHospitalRequest());
 
             } catch (Exception e) {
                 e.printStackTrace();
             }
-        }).start();
+        }).start();*/
 
 
         setMarker();
@@ -162,34 +178,24 @@ public class MapActivity extends AppCompatActivity implements MapView.CurrentLoc
         mapView.getCurrentLocationTrackingMode();
     }
 
+
     private void setMarker() {
         MapPOIItem marker = new MapPOIItem();
         marker.setItemName("Default Marker");
         marker.setTag(0);
 
-        try {
-
-            for (Item i : hospitalItems) {
-                if (!"".equals(i.getXPos()) && !"".equals(i.getYPos())) {
-                    Log.d("test2", i.getXPos());
-                    Log.d("test2", i.getYPos());
-                }
-            }
-            Log.d("test2", hospitalItems.get(1).getXPos());
-            Log.d("test2", hospitalItems.get(1).getYPos());
-        } catch (Exception e) {
-
-        }
-
-
-        MapPoint MARKER_POINT = MapPoint.mapPointWithCONGCoord(37.6132762, 127.0978309);
+        MapPoint MARKER_POINT = MapPoint.mapPointWithCONGCoord(37.6, 127.0);
+        //MapPoint MARKER_POINT = MapPoint.mapPointWithCONGCoord(127.0, 37.6);
         marker.setMapPoint(MARKER_POINT);
 
         marker.setMarkerType(MapPOIItem.MarkerType.BluePin); // 기본으로 제공하는 BluePin 마커 모양.
         marker.setSelectedMarkerType(MapPOIItem.MarkerType.RedPin); // 마커를 클릭했을때, 기본으로 제공하는 RedPin 마커 모양.
+
         marker.setCustomImageAnchor(0.5f, 1.0f);
+
         mapView.addPOIItem(marker);
     }
+
 
     private void setCustomMarker() {
         MapPOIItem customMarker = new MapPOIItem();
@@ -208,6 +214,7 @@ public class MapActivity extends AppCompatActivity implements MapView.CurrentLoc
     @Override
     public void onCurrentLocationUpdate(MapView mapView, MapPoint mapPoint, float v) {
         MapPoint.GeoCoordinate mapPointGeo = mapPoint.getMapPointGeoCoord();
+        Log.d("test", String.format("mapview oncurrentLocationUpdate (%f, %f) accuracy (%f)", mapPointGeo.latitude, mapPointGeo.longitude, v));
 
     }
 
@@ -226,4 +233,78 @@ public class MapActivity extends AppCompatActivity implements MapView.CurrentLoc
 
     }
 
+    //MapViewEventListener
+
+    @Override
+    public void onMapViewInitialized(MapView mapView) {
+        Log.i(LOG_TAG, "MapView had loaded. Now, MapView APIs could be called safely");
+        //mMapView.setCurrentLocationTrackingMode(MapView.CurrentLocationTrackingMode.TrackingModeOnWithoutHeading);
+        mapView.setMapCenterPointAndZoomLevel(MapPoint.mapPointWithGeoCoord(37.537229, 127.005515), 2, true);
+    }
+
+    @Override
+    public void onMapViewCenterPointMoved(MapView mapView, MapPoint mapPoint) {
+        MapPoint.GeoCoordinate mapPointGeo = mapPoint.getMapPointGeoCoord();
+        Log.i(LOG_TAG, String.format("MapView onMapViewCenterPointMoved (%f,%f)", mapPointGeo.latitude, mapPointGeo.longitude));
+    }
+
+    @Override
+    public void onMapViewZoomLevelChanged(MapView mapView, int i) {
+        Log.i(LOG_TAG, String.format("MapView onMapViewZoomLevelChanged (%d)", i));
+    }
+
+    @Override
+    public void onMapViewSingleTapped(MapView mapView, MapPoint mapPoint) {
+        MapPoint.GeoCoordinate mapPointGeo = mapPoint.getMapPointGeoCoord();
+        Log.i(LOG_TAG, String.format("MapView onMapViewSingleTapped (%f,%f)", mapPointGeo.latitude, mapPointGeo.longitude));
+    }
+
+    @Override
+    public void onMapViewDoubleTapped(MapView mapView, MapPoint mapPoint) {
+        MapPoint.GeoCoordinate mapPointGeo = mapPoint.getMapPointGeoCoord();
+
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
+        alertDialog.setTitle("DaumMapLibrarySample");
+        alertDialog.setMessage(String.format("Double-Tap on (%f,%f)", mapPointGeo.latitude, mapPointGeo.longitude));
+        alertDialog.setPositiveButton("OK", null);
+        alertDialog.show();
+    }
+
+    @Override
+    public void onMapViewLongPressed(MapView mapView, MapPoint mapPoint) {
+        MapPoint.GeoCoordinate mapPointGeo = mapPoint.getMapPointGeoCoord();
+
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
+        alertDialog.setTitle("DaumMapLibrarySample");
+        alertDialog.setMessage(String.format("Long-Press on (%f,%f)", mapPointGeo.latitude, mapPointGeo.longitude));
+        alertDialog.setPositiveButton("OK", null);
+        alertDialog.show();
+    }
+
+    @Override
+    public void onMapViewDragStarted(MapView mapView, MapPoint mapPoint) {
+        MapPoint.GeoCoordinate mapPointGeo = mapPoint.getMapPointGeoCoord();
+        Log.i(LOG_TAG, String.format("MapView onMapViewDragStarted (%f,%f)", mapPointGeo.latitude, mapPointGeo.longitude));
+    }
+
+    @Override
+    public void onMapViewDragEnded(MapView mapView, MapPoint mapPoint) {
+        MapPoint.GeoCoordinate mapPointGeo = mapPoint.getMapPointGeoCoord();
+        Log.i(LOG_TAG, String.format("MapView onMapViewDragEnded (%f,%f)", mapPointGeo.latitude, mapPointGeo.longitude));
+    }
+
+    @Override
+    public void onMapViewMoveFinished(MapView mapView, MapPoint mapPoint) {
+        MapPoint.GeoCoordinate mapPointGeo = mapPoint.getMapPointGeoCoord();
+        Log.i(LOG_TAG, String.format("MapView onMapViewMoveFinished (%f,%f)", mapPointGeo.latitude, mapPointGeo.longitude));
+    }
+
+    //OpenAPIKeyAuthenticationResultListener
+
+    @Override
+    public void onDaumMapOpenAPIKeyAuthenticationResult(MapView mapView, int resultCode, String resultMessage) {
+        Log.i("test", String.format("Open API Key Authentication Result : code=%d, message=%s", resultCode, resultMessage));
+    }
 }
+
+
