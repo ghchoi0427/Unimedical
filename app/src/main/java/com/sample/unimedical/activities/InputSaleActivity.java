72c5ee85709159b4b7c6dda62bc59627879b2b6d
choi <ghchoi0427@gmail.com>
2021-07-21 11:50:28 +0900

    feat: 판매 입력 액티비티 생성

---
+++ app/src/main/java/com/sample/unimedical/activities/InputSaleActivity.java
@@ -0,0 +1,16 @@
+package com.sample.unimedical.activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.sample.unimedical.R;
+
+public class InputSaleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_input_sale);
+    }
+}
