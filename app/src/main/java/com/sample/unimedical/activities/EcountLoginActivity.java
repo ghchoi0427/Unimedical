1ae2e3c98ad4cdca11cac6a6de89cb1fee4fbff5
choi <ghchoi0427@gmail.com>
2021-07-22 10:19:23 +0900

    feat: 이카운트 로그인 테스트 액티비티

---
+++ app/src/main/java/com/sample/unimedical/activities/EcountLoginActivity.java
@@ -0,0 +1,43 @@
+package com.sample.unimedical.activities;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.sample.unimedical.R;
+
+import static com.sample.unimedical.util.RequestSender.sendEcountLoginRequest;
+
+public class EcountLoginActivity extends AppCompatActivity {
+
+    EditText editComCode;
+    EditText editUserID;
+    Button ecountLogin;
+    TextView textEcountLoginResult;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ecount_login);
+
+        editComCode = findViewById(R.id.edit_com_code);
+        editUserID = findViewById(R.id.edit_user_id);
+        ecountLogin = findViewById(R.id.btn_ecount_login);
+        textEcountLoginResult = findViewById(R.id.text_ecount_login_result);
+
+        ecountLogin.setOnClickListener(view -> new Thread(() -> {
+
+            try {
+                textEcountLoginResult.setText(sendEcountLoginRequest("310316", "양예림"));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        }).start());
+
+    }
+}
