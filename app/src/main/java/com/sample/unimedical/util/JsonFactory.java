34c4cc75a3db93fc622eeffd67106ce723a0cd15
choi <ghchoi0427@gmail.com>
2021-07-22 16:59:09 +0900

    refactor: 키 이름 변경

--- app/src/main/java/com/sample/unimedical/util/JsonFactory.java
+++ app/src/main/java/com/sample/unimedical/util/JsonFactory.java
@@ -5,14 +5,14 @@ import org.json.JSONObject;
 
 public class JsonFactory {
 
-    private static final String ECOUNT_API_KEY = "3e533429870154db68acac0e662825e193";
+    private static final String ECOUNT_API_KEY_TEST = "3e533429870154db68acac0e662825e193";
     private static final String WH_CD = "00001";
 
     public static JSONObject createLoginJSONObject(String comCode, String userID) throws JSONException {
         return new JSONObject()
                 .put("COM_CODE", comCode)
                 .put("USER_ID", userID)
-                .put("API_CERT_KEY", ECOUNT_API_KEY)
+                .put("API_CERT_KEY", ECOUNT_API_KEY_TEST)
                 .put("LAN_TYPE", "ko-KR")
                 .put("ZONE", "BA");
     }
