0cfe93986d07db26774e7f0cc6252906e07e46e1
choi <ghchoi0427@gmail.com>
2021-07-23 11:22:27 +0900

    feat: sale item 어댑터 생성

---
+++ app/src/main/java/com/sample/unimedical/adapter/SaleAdapter.java
@@ -0,0 +1,65 @@
+package com.sample.unimedical.adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.sample.unimedical.R;
+import com.sample.unimedical.domain.sale.SaleItem;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+
+public class SaleAdapter extends RecyclerView.Adapter<SaleAdapter.ViewHolder> {
+    ArrayList<SaleItem> saleItems = new ArrayList<>();
+
+    @NonNull
+    @NotNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull @NotNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View saleView = inflater.inflate(R.layout.sale_item, parent, false);
+
+        return new ViewHolder(saleView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull @NotNull SaleAdapter.ViewHolder holder, int position) {
+        SaleItem saleItem = saleItems.get(position);
+        holder.setItem(saleItem);
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    static class ViewHolder extends RecyclerView.ViewHolder {
+
+        TextView textSaleItemCode;
+        TextView textSaleItemQuantity;
+
+        public ViewHolder(@NonNull @NotNull View itemView) {
+            super(itemView);
+
+            textSaleItemCode = itemView.findViewById(R.id.text_sale_item_code);
+            textSaleItemQuantity = itemView.findViewById(R.id.text_sale_item_quantity);
+        }
+
+        public void setItem(SaleItem saleItem) {
+            try {
+                textSaleItemCode.setText("");
+                textSaleItemQuantity.setText("");
+            } catch (Exception e) {
+
+            }
+        }
+    }
+
+
+}
