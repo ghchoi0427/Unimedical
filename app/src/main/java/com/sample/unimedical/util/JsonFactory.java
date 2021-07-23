6d2350ffe953bd6112079147aaaa9ac3be55d1f7
choi <ghchoi0427@gmail.com>
2021-07-23 10:21:53 +0900

    feat: JSON 생성 메서드

--- app/src/main/java/com/sample/unimedical/util/JsonFactory.java
+++ app/src/main/java/com/sample/unimedical/util/JsonFactory.java
@@ -1,5 +1,8 @@
 package com.sample.unimedical.util;
 
+import android.util.Log;
+
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -8,6 +11,7 @@ public class JsonFactory {
     private static final String ECOUNT_API_KEY_TEST = "3e533429870154db68acac0e662825e193";
     private static final String ECOUNT_API_KEY = "1952f97f0a12f48f690c708ff9b28fe0b1";
     private static final String WH_CD = "00001";
+
 
     public static JSONObject getZoneJSONObject(String comCode) throws JSONException {
         return new JSONObject()
@@ -38,4 +42,28 @@ public class JsonFactory {
                 "}";
         return new JSONObject(str);
     }
+
+    public static JSONObject saleItem(String PROD_CD, String REMARKS, String PROD_DES, String SIZE_DES, String QTY, String PRICE) throws JSONException {
+        JSONObject Sale = new JSONObject();
+        JSONObject BulkDatas = new JSONObject();
+
+        BulkDatas.put("PROD_CD", PROD_CD)
+                .put("REMARKS", REMARKS)
+                .put("PROD_DES", PROD_DES)
+                .put("SIZE_DES", SIZE_DES)
+                .put("QTY", QTY)
+                .put("PRICE", PRICE);
+
+        Sale.put("Line", "0");
+        Sale.put("BulkDatas", BulkDatas);
+
+        Log.d("tester", Sale.toString());
+
+        return Sale;
+    }
+
+    public static JSONObject saleList(JSONArray salesItems) throws JSONException {
+        return new JSONObject().put("SaleList", salesItems);
+    }
+
 }
