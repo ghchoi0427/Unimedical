ddc9fda6b8b9c6e5856870d3d6313ae1161a2013
choi <ghchoi0427@gmail.com>
2021-07-23 15:28:13 +0900

    feat: 검색어 입력

--- app/src/main/java/com/sample/unimedical/activities/InputSaleActivity.java
+++ app/src/main/java/com/sample/unimedical/activities/InputSaleActivity.java
@@ -11,7 +11,9 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.sample.unimedical.R;
 import com.sample.unimedical.adapter.SaleAdapter;
 
+import org.json.JSONArray;
 import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.IOException;
 
@@ -20,24 +22,38 @@ import static com.sample.unimedical.util.RequestSender.sendEcountInputSaleReques
 
 public class InputSaleActivity extends AppCompatActivity {
 
+    EditText uploadDate;
+    EditText employeeCode;
+    EditText ioType;
     EditText uploadSerialNumber;
     EditText productCode;
     EditText quantity;
+    EditText price;
+    EditText remarks;
+
     Button uploadSale;
     Button addSaleItem;
     SaleAdapter saleAdapter;
     RecyclerView recyclerViewSale;
     String SESSION_ID;
     String ZONE_CODE = "";
+    JSONArray jsonArray = new JSONArray();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_input_sale);
 
+        uploadDate = findViewById(R.id.edit_upload_date);
+        employeeCode = findViewById(R.id.edit_employee_code);
+        ioType = findViewById(R.id.edit_io_type);
         uploadSerialNumber = findViewById(R.id.edit_upload_ser_no);
         productCode = findViewById(R.id.edit_prod_cd);
         quantity = findViewById(R.id.edit_qty);
+        price = findViewById(R.id.edit_price);
+        remarks = findViewById(R.id.edit_remarks);
+
+
         uploadSale = findViewById(R.id.btn_upload_sale);
         addSaleItem = findViewById(R.id.btn_add_sale_item);
         saleAdapter = new SaleAdapter();
@@ -53,7 +69,16 @@ public class InputSaleActivity extends AppCompatActivity {
 
         addSaleItem.setOnClickListener(v -> {
             try {
-                saleAdapter.addItem(createSaleItem("00070","none","product name","size asdf","quantity","1234"));
+                JSONObject individualSaleItem = createSaleItem(
+                        uploadSerialNumber.getText().toString(),
+                        productCode.getText().toString(),
+                        remarks.getText().toString(),
+                        quantity.getText().toString(),
+                        price.getText().toString());
+
+                saleAdapter.addItem(individualSaleItem);
+                jsonArray.put(individualSaleItem);
+
             } catch (JSONException e) {
                 e.printStackTrace();
             }
