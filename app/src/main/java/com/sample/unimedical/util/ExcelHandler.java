3c5742af96519e0d21c75f1df92469ecb8a223f0
choi <ghchoi0427@gmail.com>
2021-07-26 14:37:42 +0900

    fix: method return type 변경

--- app/src/main/java/com/sample/unimedical/util/ExcelHandler.java
+++ app/src/main/java/com/sample/unimedical/util/ExcelHandler.java
@@ -1,12 +1,15 @@
 package com.sample.unimedical.util;
 
 import android.content.Context;
-import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.sample.unimedical.domain.hospital.Hospital;
+
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 import jxl.Sheet;
 import jxl.Workbook;
@@ -14,7 +17,9 @@ import jxl.read.biff.BiffException;
 
 public class ExcelHandler extends AppCompatActivity {
 
-    public void readExcel(Context context) {
+    public List<Hospital> searchContracts(Context context, String keyword) {
+        List<Hospital> searchList = new ArrayList<>();
+
         try {
             InputStream is = context.getResources().getAssets().open("account0726.xls");
             Workbook wb = Workbook.getWorkbook(is);
@@ -28,9 +33,11 @@ public class ExcelHandler extends AppCompatActivity {
 
                 for (int row = rowIndexStart; row < rowTotal; row++) {
 
-                    String content = sheet.getCell(4, row).getContents();
-                    if (validateHospital(content)) {
-                        getHospitalInfo(sheet, row);
+                    String hospitalName = sheet.getCell(4, row).getContents();
+                    if (validateHospital(hospitalName)) {
+                        if (hospitalName.equals(keyword)) {
+                            searchList.add(getHospitalInfo(sheet, row));
+                        }
                     }
                 }
             }
@@ -38,18 +45,22 @@ public class ExcelHandler extends AppCompatActivity {
         } catch (IOException | BiffException e) {
             e.printStackTrace();
         }
+        return searchList;
     }
 
-    public boolean validateHospital(String hospitalCell) {
+    private boolean validateHospital(String hospitalCell) {
         if (hospitalCell.contains("폐업") || hospitalCell.contains("거래중지") || hospitalCell.contains("(주)") || hospitalCell.contains("주식회사") || hospitalCell.equals("")) {
             return false;
         }
         return true;
     }
 
-    private void getHospitalInfo(Sheet sheet, int row) {
-        String hospitalName = sheet.getCell(4, row).getContents();
-        String manager = sheet.getCell(1, row).getContents();
-        String device = sheet.getCell(9, row).getContents();
+    private Hospital getHospitalInfo(Sheet sheet, int row) {
+        Hospital hospital = new Hospital();
+        hospital.setYadmNm(sheet.getCell(4, row).getContents());
+        hospital.setManager(sheet.getCell(1, row).getContents());
+        hospital.setDevice(sheet.getCell(9, row).getContents());
+
+        return hospital;
     }
 }
