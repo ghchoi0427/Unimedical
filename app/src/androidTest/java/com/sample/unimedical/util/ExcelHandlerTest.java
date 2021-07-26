c05584ae56a6e3bc1489ec10e503831d5efe60c1
choi <ghchoi0427@gmail.com>
2021-07-26 14:36:58 +0900

    refactor: test 삭제

---
+++ app/src/androidTest/java/com/sample/unimedical/util/ExcelHandlerTest.java
@@ -0,0 +1,16 @@
+package com.sample.unimedical.util;
+
+import android.util.Log;
+
+import junit.framework.TestCase;
+
+import org.junit.Test;
+
+public class ExcelHandlerTest extends TestCase {
+
+    @Test
+    public void testLoadExcel() {
+
+    }
+
+}
