18f11443fb76887a8a9a9628d9722b1851c4e320
choi <ghchoi0427@gmail.com>
2021-07-28 17:50:18 +0900

    feat: StatusList 클래스 생성

---
+++ app/src/main/java/com/sample/unimedical/domain/status/StatusList.java
@@ -0,0 +1,15 @@
+package com.sample.unimedical.domain.status;
+
+import java.util.ArrayList;
+
+public class StatusList {
+    private ArrayList<Status> statuses;
+
+    public ArrayList<Status> getStatuses() {
+        return statuses;
+    }
+
+    public void setStatuses(ArrayList<Status> statuses) {
+        this.statuses = statuses;
+    }
+}
