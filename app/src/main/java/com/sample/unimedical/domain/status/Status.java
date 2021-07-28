368a0ee9c60fe0fc652324e7d667fa7fc1bccc2a
choi <ghchoi0427@gmail.com>
2021-07-28 13:50:29 +0900

    feat: status 클래스 생성

---
+++ app/src/main/java/com/sample/unimedical/domain/status/Status.java
@@ -0,0 +1,373 @@
+package com.sample.unimedical.domain.status;
+
+public class Status {
+    private String number;
+    private String hospitalName;
+    private String telNo;
+    private String address;
+    private String city;
+    private String function;
+    private String district;
+    private String function2;
+    private String assessment;
+    private String shaver_date_visit;
+    private String shaver_incharge;
+    private String shaver_interest;
+    private String main_remark;
+    private String shaver_introduction;
+    private String shaver_type;
+    private String shaver_salesman;
+    private String shaver_visit_salesman;
+    private String shaver_middle_contractor;
+    private String shaver_quotation_price;
+    private String shaver_contract_price;
+    private String shaver_sample_presence;
+    private String shaver_date_sampling;
+    private String shaver_result_sampling;
+    private String shaver_remark;
+    private String shaver_count_visit;
+    private String qplint_introduction;
+    private String qplint_salesman;
+    private String qplint_date_sales_visit;
+    private String qplint_date_last_visit;
+    private String qplint_interest;
+    private String qplint_result_sale;
+    private String patient;
+    private String hospitalKey;
+    private String remark;
+    private String coolband_introduction;
+    private String coolband_salesman;
+    private String coolband_salesvisit;
+    private String coolband_date_last_visit;
+    private String coolband_result_sample;
+    private String coolband_remark;
+    private String area;
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getHospitalName() {
+        return hospitalName;
+    }
+
+    public void setHospitalName(String hospitalName) {
+        this.hospitalName = hospitalName;
+    }
+
+    public String getTelNo() {
+        return telNo;
+    }
+
+    public void setTelNo(String telNo) {
+        this.telNo = telNo;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getFunction() {
+        return function;
+    }
+
+    public void setFunction(String function) {
+        this.function = function;
+    }
+
+    public String getDistrict() {
+        return district;
+    }
+
+    public void setDistrict(String district) {
+        this.district = district;
+    }
+
+    public String getFunction2() {
+        return function2;
+    }
+
+    public void setFunction2(String function2) {
+        this.function2 = function2;
+    }
+
+    public String getAssessment() {
+        return assessment;
+    }
+
+    public void setAssessment(String assessment) {
+        this.assessment = assessment;
+    }
+
+    public String getShaver_date_visit() {
+        return shaver_date_visit;
+    }
+
+    public void setShaver_date_visit(String shaver_date_visit) {
+        this.shaver_date_visit = shaver_date_visit;
+    }
+
+    public String getShaver_incharge() {
+        return shaver_incharge;
+    }
+
+    public void setShaver_incharge(String shaver_incharge) {
+        this.shaver_incharge = shaver_incharge;
+    }
+
+    public String getShaver_interest() {
+        return shaver_interest;
+    }
+
+    public void setShaver_interest(String shaver_interest) {
+        this.shaver_interest = shaver_interest;
+    }
+
+    public String getMain_remark() {
+        return main_remark;
+    }
+
+    public void setMain_remark(String main_remark) {
+        this.main_remark = main_remark;
+    }
+
+    public String getShaver_introduction() {
+        return shaver_introduction;
+    }
+
+    public void setShaver_introduction(String shaver_introduction) {
+        this.shaver_introduction = shaver_introduction;
+    }
+
+    public String getShaver_type() {
+        return shaver_type;
+    }
+
+    public void setShaver_type(String shaver_type) {
+        this.shaver_type = shaver_type;
+    }
+
+    public String getShaver_salesman() {
+        return shaver_salesman;
+    }
+
+    public void setShaver_salesman(String shaver_salesman) {
+        this.shaver_salesman = shaver_salesman;
+    }
+
+    public String getShaver_visit_salesman() {
+        return shaver_visit_salesman;
+    }
+
+    public void setShaver_visit_salesman(String shaver_visit_salesman) {
+        this.shaver_visit_salesman = shaver_visit_salesman;
+    }
+
+    public String getShaver_middle_contractor() {
+        return shaver_middle_contractor;
+    }
+
+    public void setShaver_middle_contractor(String shaver_middle_contractor) {
+        this.shaver_middle_contractor = shaver_middle_contractor;
+    }
+
+    public String getShaver_quotation_price() {
+        return shaver_quotation_price;
+    }
+
+    public void setShaver_quotation_price(String shaver_quotation_price) {
+        this.shaver_quotation_price = shaver_quotation_price;
+    }
+
+    public String getShaver_contract_price() {
+        return shaver_contract_price;
+    }
+
+    public void setShaver_contract_price(String shaver_contract_price) {
+        this.shaver_contract_price = shaver_contract_price;
+    }
+
+    public String getShaver_sample_presence() {
+        return shaver_sample_presence;
+    }
+
+    public void setShaver_sample_presence(String shaver_sample_presence) {
+        this.shaver_sample_presence = shaver_sample_presence;
+    }
+
+    public String getShaver_date_sampling() {
+        return shaver_date_sampling;
+    }
+
+    public void setShaver_date_sampling(String shaver_date_sampling) {
+        this.shaver_date_sampling = shaver_date_sampling;
+    }
+
+    public String getShaver_result_sampling() {
+        return shaver_result_sampling;
+    }
+
+    public void setShaver_result_sampling(String shaver_result_sampling) {
+        this.shaver_result_sampling = shaver_result_sampling;
+    }
+
+    public String getShaver_remark() {
+        return shaver_remark;
+    }
+
+    public void setShaver_remark(String shaver_remark) {
+        this.shaver_remark = shaver_remark;
+    }
+
+    public String getShaver_count_visit() {
+        return shaver_count_visit;
+    }
+
+    public void setShaver_count_visit(String shaver_count_visit) {
+        this.shaver_count_visit = shaver_count_visit;
+    }
+
+    public String getQplint_introduction() {
+        return qplint_introduction;
+    }
+
+    public void setQplint_introduction(String qplint_introduction) {
+        this.qplint_introduction = qplint_introduction;
+    }
+
+    public String getQplint_salesman() {
+        return qplint_salesman;
+    }
+
+    public void setQplint_salesman(String qplint_salesman) {
+        this.qplint_salesman = qplint_salesman;
+    }
+
+    public String getQplint_date_sales_visit() {
+        return qplint_date_sales_visit;
+    }
+
+    public void setQplint_date_sales_visit(String qplint_date_sales_visit) {
+        this.qplint_date_sales_visit = qplint_date_sales_visit;
+    }
+
+    public String getQplint_date_last_visit() {
+        return qplint_date_last_visit;
+    }
+
+    public void setQplint_date_last_visit(String qplint_date_last_visit) {
+        this.qplint_date_last_visit = qplint_date_last_visit;
+    }
+
+    public String getQplint_interest() {
+        return qplint_interest;
+    }
+
+    public void setQplint_interest(String qplint_interest) {
+        this.qplint_interest = qplint_interest;
+    }
+
+    public String getQplint_result_sale() {
+        return qplint_result_sale;
+    }
+
+    public void setQplint_result_sale(String qplint_result_sale) {
+        this.qplint_result_sale = qplint_result_sale;
+    }
+
+    public String getPatient() {
+        return patient;
+    }
+
+    public void setPatient(String patient) {
+        this.patient = patient;
+    }
+
+    public String getHospitalKey() {
+        return hospitalKey;
+    }
+
+    public void setHospitalKey(String hospitalKey) {
+        this.hospitalKey = hospitalKey;
+    }
+
+    public String getRemark() {
+        return remark;
+    }
+
+    public void setRemark(String remark) {
+        this.remark = remark;
+    }
+
+    public String getCoolband_introduction() {
+        return coolband_introduction;
+    }
+
+    public void setCoolband_introduction(String coolband_introduction) {
+        this.coolband_introduction = coolband_introduction;
+    }
+
+    public String getCoolband_salesman() {
+        return coolband_salesman;
+    }
+
+    public void setCoolband_salesman(String coolband_salesman) {
+        this.coolband_salesman = coolband_salesman;
+    }
+
+    public String getCoolband_salesvisit() {
+        return coolband_salesvisit;
+    }
+
+    public void setCoolband_salesvisit(String coolband_salesvisit) {
+        this.coolband_salesvisit = coolband_salesvisit;
+    }
+
+    public String getCoolband_date_last_visit() {
+        return coolband_date_last_visit;
+    }
+
+    public void setCoolband_date_last_visit(String coolband_date_last_visit) {
+        this.coolband_date_last_visit = coolband_date_last_visit;
+    }
+
+    public String getCoolband_result_sample() {
+        return coolband_result_sample;
+    }
+
+    public void setCoolband_result_sample(String coolband_result_sample) {
+        this.coolband_result_sample = coolband_result_sample;
+    }
+
+    public String getCoolband_remark() {
+        return coolband_remark;
+    }
+
+    public void setCoolband_remark(String coolband_remark) {
+        this.coolband_remark = coolband_remark;
+    }
+
+    public String getArea() {
+        return area;
+    }
+
+    public void setArea(String area) {
+        this.area = area;
+    }
+}
