8be324904b2c02cb57e7ea9a01f2736b0cdb0a1d
choi <ghchoi0427@gmail.com>
2021-08-02 14:05:36 +0900

    fix: 파일명 선언

--- app/src/main/java/com/sample/unimedical/util/FirebaseHandler.java
+++ app/src/main/java/com/sample/unimedical/util/FirebaseHandler.java
@@ -5,24 +5,21 @@ import android.content.Context;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
-import java.io.BufferedReader;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.nio.charset.StandardCharsets;
 
 public class FirebaseHandler {
 
     private static FirebaseStorage storage = FirebaseStorage.getInstance("gs://medi-5dbf9.appspot.com/");
     private static final String CUSTOMER_FILE_NAME = "customer.xls";
+    private static final String LOCAL_CUSTOMER_FILE_NAME = "local_customer.xls";
 
     public static void downloadFile(Context context) throws FileNotFoundException {
         StorageReference storageReference = storage.getReference();
         StorageReference customerRef = storageReference.child(CUSTOMER_FILE_NAME);
 
-        FileOutputStream fos = context.openFileOutput("test_file.xls", Context.MODE_PRIVATE);
+        FileOutputStream fos = context.openFileOutput(LOCAL_CUSTOMER_FILE_NAME, Context.MODE_PRIVATE);
 
         final long ONE_MEGABYTE = 1024 * 1024;
 
@@ -34,22 +31,8 @@ public class FirebaseHandler {
             } catch (IOException e) {
                 e.printStackTrace();
             }
-        }).addOnCompleteListener(task -> {
-            try {
-                readFile(context);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
         });
     }
 
-    public static void readFile(Context context) throws IOException {
-        FileInputStream fis = context.openFileInput("test_file.xls");
-        BufferedReader br = new BufferedReader(new InputStreamReader(fis, StandardCharsets.UTF_8));
-        StringBuilder sb = new StringBuilder();
-        String line;
-        while ((line = br.readLine()) != null) {
-            sb.append(line);
-        }
-    }
+
 }
