1c833d2c7192c1b6083c530334370a7f33081c0a
choi <ghchoi0427@gmail.com>
2021-08-02 11:53:16 +0900

    feat: 파이어베이스에서 내부 저장소로 다운로드

--- app/src/main/java/com/sample/unimedical/util/FirebaseHandler.java
+++ app/src/main/java/com/sample/unimedical/util/FirebaseHandler.java
@@ -1,11 +1,13 @@
 package com.sample.unimedical.util;
 
+import android.content.Context;
 import android.util.Log;
 
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
-import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class FirebaseHandler {
@@ -13,15 +15,26 @@ public class FirebaseHandler {
     private static FirebaseStorage storage = FirebaseStorage.getInstance("gs://medi-5dbf9.appspot.com/");
     private static final String CUSTOMER_FILE_NAME = "customer.xls";
 
-    public static void test() throws IOException {
+    public static void downloadFile(Context context) throws FileNotFoundException {
         StorageReference storageReference = storage.getReference();
         StorageReference customerRef = storageReference.child(CUSTOMER_FILE_NAME);
 
-        File localCustomerFile = File.createTempFile("customer", "xls");
-        customerRef.getFile(localCustomerFile)
-                .addOnSuccessListener(e -> Log.d("tester", "file load succeed"))
-                .addOnFailureListener(e -> Log.d("tester", "fail"));
+        FileOutputStream fos = context.openFileOutput("test_file.xls", Context.MODE_PRIVATE);
 
+        final long ONE_MEGABYTE = 1024 * 1024;
 
+        Log.d("tester", "initiated");
+        customerRef.getBytes(ONE_MEGABYTE).addOnSuccessListener(bytes -> {
+            try {
+                Log.d("tester", "succeed");
+                fos.write(bytes);
+                fos.flush();
+                fos.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }).addOnFailureListener(e -> {
+            Log.d("tester", "fail");
+        });
     }
 }
