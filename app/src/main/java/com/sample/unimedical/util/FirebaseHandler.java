c9f4a46c975fa833830f5e12b2e4e7c96721c760
choi <ghchoi0427@gmail.com>
2021-08-02 15:29:46 +0900

    feat: 파이어베이스에서 병원 정보 받아오기

--- app/src/main/java/com/sample/unimedical/util/FirebaseHandler.java
+++ app/src/main/java/com/sample/unimedical/util/FirebaseHandler.java
@@ -33,5 +33,24 @@ public class FirebaseHandler {
         }).addOnCompleteListener(v -> Toast.makeText(context, "거래처 정보 업데이트가 완료되었습니다.", Toast.LENGTH_SHORT).show());
     }
 
+    public static void downloadHospitalFile(Context context, String localHospitalFileName, String hospitalFileName) throws FileNotFoundException {
+        StorageReference storageReference = storage.getReference();
+        StorageReference customerRef = storageReference.child(hospitalFileName);
+
+        FileOutputStream fos = context.openFileOutput(localHospitalFileName, Context.MODE_PRIVATE);
+
+        final long ONE_MEGABYTE = 1024 * 1024;
+
+        customerRef.getBytes(ONE_MEGABYTE).addOnSuccessListener(bytes -> {
+            try {
+                fos.write(bytes);
+                fos.flush();
+                fos.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }).addOnCompleteListener(v -> Toast.makeText(context, "병원 정보 업데이트가 완료되었습니다.", Toast.LENGTH_SHORT).show());
+    }
+
 
 }
