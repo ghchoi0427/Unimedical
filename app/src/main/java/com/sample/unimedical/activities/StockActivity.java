16bf342c7261c1fc9bd029405f3edd6852a7d6c4
choi <ghchoi0427@gmail.com>
2021-08-03 15:13:47 +0900

    feat: 인텐트 통해 session id값 넘겨받기

--- app/src/main/java/com/sample/unimedical/activities/StockActivity.java
+++ app/src/main/java/com/sample/unimedical/activities/StockActivity.java
@@ -7,11 +7,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.sample.unimedical.R;
 
 public class StockActivity extends AppCompatActivity {
+    private String SESSION_ID = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stock);
+
+        SESSION_ID = getIntent().getStringExtra("SESSION_ID");
 
     }
 }
