da9fea5be9442ce1b149d88345bdf74f72ed930f
choi <ghchoi0427@gmail.com>
2021-08-04 15:42:28 +0900

    feat: 로그인 기능 (simple)

--- app/src/main/java/com/sample/unimedical/activities/LoginActivity.java
+++ app/src/main/java/com/sample/unimedical/activities/LoginActivity.java
@@ -2,70 +2,44 @@ package com.sample.unimedical.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.ImageButton;
-import android.widget.Toast;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.kakao.sdk.user.UserApiClient;
-import com.kakao.sdk.user.model.Account;
 import com.sample.unimedical.R;
 
 public class LoginActivity extends AppCompatActivity {
-    private static final String TAG = "사용자";
-    private ImageButton btnLogin, btnLogOut;
+
+    EditText editPassword;
+    Button btnLogin;
+    Button btnVisitorLogin;
+    String password = "1234";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        btnLogin = findViewById(R.id.btnLogin);
-        btnLogOut = findViewById(R.id.btnLogout);
+        btnLogin = findViewById(R.id.btn_login);
+        btnVisitorLogin = findViewById(R.id.btn_login_visitor);
+        editPassword = findViewById(R.id.edit_login_code);
 
-
-        btnLogin.setOnClickListener(v -> UserApiClient.getInstance().loginWithKakaoTalk(LoginActivity.this, (oAuthToken, error) -> {
-            if (error != null) {
-                Log.e(TAG, "로그인 실패", error);
-                Toast.makeText(getApplicationContext(), "Login failure", Toast.LENGTH_SHORT).show();
-            } else if (oAuthToken != null) {
-                Log.i(TAG, "로그인 성공(토큰) : " + oAuthToken.getAccessToken());
-
-                UserApiClient.getInstance().me((user, meError) -> {
-                    if (meError != null) {
-                        Log.e(TAG, "사용자 정보 요청 실패", meError);
-                    } else {
-                        System.out.println("로그인 완료");
-                        Toast.makeText(getApplicationContext(), "Login complete", Toast.LENGTH_SHORT).show();
-                        Log.i(TAG, user.toString());
-                        {
-                            Log.i(TAG, "사용자 정보 요청 성공" +
-                                    "\n회원번호: " + user.getId() +
-                                    "\n이메일: " + user.getKakaoAccount().getEmail());
-                        }
-                        Account user1 = user.getKakaoAccount();
-                        System.out.println("사용자 계정" + user1);
-
-                    }
-                    return null;
-                });
-                //로그인 성공 시 화면 전환
-                startActivity(new Intent(LoginActivity.this, MainActivity.class));
-
+        btnLogin.setOnClickListener(v -> {
+            if (validatePassword(editPassword.getText().toString().trim())) {
+                startActivity(new Intent(this, MainActivity.class).putExtra("authentification", "true"));
+                finish();
             }
-            return null;
-        }));
+        });
 
-        btnLogOut.setOnClickListener(v -> UserApiClient.getInstance().logout(error -> {
-            if (error != null) {
-                Log.e(TAG, "로그아웃 실패, SDK에서 토큰 삭제됨", error);
-                Toast.makeText(getApplicationContext(), "Log Out failure", Toast.LENGTH_SHORT).show();
-            } else {
-                Log.e(TAG, "로그아웃 성공, SDK에서 토큰 삭제됨");
-                Toast.makeText(getApplicationContext(), "Log Out complete", Toast.LENGTH_SHORT).show();
-            }
-            return null;
-        }));
+        btnVisitorLogin.setOnClickListener(v -> {
+            startActivity(new Intent(this, MainActivity.class).putExtra("authentification", "false"));
+            finish();
+        });
+
+    }
+
+    private boolean validatePassword(String input) {
+        return input.equals(password);
     }
 }
