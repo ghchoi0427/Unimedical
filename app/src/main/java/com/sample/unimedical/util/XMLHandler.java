bc83859912c311ceda6b1404fecd71afa04ea27b
choi <ghchoi0427@gmail.com>
2021-08-05 13:41:49 +0900

    fix: 접근제한자 변경

--- app/src/main/java/com/sample/unimedical/util/XMLHandler.java
+++ app/src/main/java/com/sample/unimedical/util/XMLHandler.java
@@ -126,7 +126,7 @@ public class XMLHandler {
         }
     }
 
-    private static String readTextFile(InputStream inputStream) {
+    public static String readTextFile(InputStream inputStream) {
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
 
         byte[] buf = new byte[1024];
