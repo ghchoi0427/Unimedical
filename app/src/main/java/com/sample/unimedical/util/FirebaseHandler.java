2623fb53c7328d6569df1a9eff3bd3873496bec5
choi <ghchoi0427@gmail.com>
2021-08-05 15:57:25 +0900

    refactor: 중복된 메서드 제거

--- app/src/main/java/com/sample/unimedical/util/FirebaseHandler.java
+++ app/src/main/java/com/sample/unimedical/util/FirebaseHandler.java
@@ -14,7 +14,7 @@ public class FirebaseHandler {
 
     private static FirebaseStorage storage = FirebaseStorage.getInstance("gs://medi-5dbf9.appspot.com/");
 
-    public static void downloadCustomerFile(Context context, String localCustomerFileName, String customerFileName) throws FileNotFoundException {
+    public static void downloadFile(Context context, String localCustomerFileName, String customerFileName) throws FileNotFoundException {
         StorageReference storageReference = storage.getReference();
         StorageReference customerRef = storageReference.child(customerFileName);
 
@@ -33,25 +33,5 @@ public class FirebaseHandler {
         }).addOnFailureListener(v -> Toast.makeText(context, "정보 업데이트 실패", Toast.LENGTH_SHORT).show())
                 .addOnCompleteListener(v -> Toast.makeText(context, "정보 업데이트가 완료되었습니다.", Toast.LENGTH_SHORT).show());
     }
-
-    public static void downloadHospitalFile(Context context, String localHospitalFileName, String hospitalFileName) throws FileNotFoundException {
-        StorageReference storageReference = storage.getReference();
-        StorageReference customerRef = storageReference.child(hospitalFileName);
-
-        FileOutputStream fos = context.openFileOutput(localHospitalFileName, Context.MODE_PRIVATE);
-
-        final long TEN_MEGABYTE = 1024 * 1024 * 10;
-
-        customerRef.getBytes(TEN_MEGABYTE).addOnSuccessListener(bytes -> {
-            try {
-                fos.write(bytes);
-                fos.flush();
-                fos.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }).addOnCompleteListener(v -> Toast.makeText(context, "병원 정보 업데이트가 완료되었습니다.", Toast.LENGTH_SHORT).show());
-    }
-
 
 }
