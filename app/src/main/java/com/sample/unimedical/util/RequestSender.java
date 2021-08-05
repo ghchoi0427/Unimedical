fc7c07d2a1fe1f1e2c5f582adabfd486e2b5c335
choi <ghchoi0427@gmail.com>
2021-08-05 10:21:19 +0900

    refactor: 불필요한 import 삭제

--- app/src/main/java/com/sample/unimedical/util/RequestSender.java
+++ app/src/main/java/com/sample/unimedical/util/RequestSender.java
@@ -12,7 +12,6 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.net.URLEncoder;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Locale;
