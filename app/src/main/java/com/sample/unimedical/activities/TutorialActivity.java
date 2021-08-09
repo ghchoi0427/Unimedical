1fef3a3503d4ba46ca85255c3a85a321d07c6f89
choi <ghchoi0427@gmail.com>
2021-08-09 15:29:27 +0900

    feat: TutorialActivity 생성

---
+++ app/src/main/java/com/sample/unimedical/activities/TutorialActivity.java
@@ -0,0 +1,17 @@
+package com.sample.unimedical.activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.sample.unimedical.R;
+
+public class TutorialActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tutorial);
+
+
+    }
+}
