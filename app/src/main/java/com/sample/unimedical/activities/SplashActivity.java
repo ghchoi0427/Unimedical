a95334d5dcd81cb242242d6c86b121a2628a6329
choi <ghchoi0427@gmail.com>
2021-08-10 14:09:42 +0900

    fix: 구식 API level에서 지원되지 않는 함수 변경

--- app/src/main/java/com/sample/unimedical/activities/SplashActivity.java
+++ app/src/main/java/com/sample/unimedical/activities/SplashActivity.java
@@ -2,8 +2,6 @@ package com.sample.unimedical.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Looper;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -15,8 +13,14 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-
-        Handler.createAsync(Looper.myLooper()).postDelayed((Runnable) () -> startActivity(new Intent(getApplicationContext(), LoginActivity.class)), 1000);
-
+        runOnUiThread(()-> {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+        startActivity(new Intent(this, LoginActivity.class));
+        finish();
     }
 }
