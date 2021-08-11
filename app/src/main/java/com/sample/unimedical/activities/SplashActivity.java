894c3f3c48c2bec464f5530233060b4cb7098d1d
choi <ghchoi0427@gmail.com>
2021-08-11 14:04:55 +0900

    fix: sleep 삭제

--- app/src/main/java/com/sample/unimedical/activities/SplashActivity.java
+++ app/src/main/java/com/sample/unimedical/activities/SplashActivity.java
@@ -13,13 +13,6 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-        runOnUiThread(()-> {
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        });
         startActivity(new Intent(this, LoginActivity.class));
         finish();
     }
