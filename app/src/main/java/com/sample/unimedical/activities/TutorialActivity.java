a72112c4a7b3427b1a73bb5bd48f253fd3e809e0
choi <ghchoi0427@gmail.com>
2021-08-12 13:09:13 +0900

    refactor: 지역변수로 선언

--- app/src/main/java/com/sample/unimedical/activities/TutorialActivity.java
+++ app/src/main/java/com/sample/unimedical/activities/TutorialActivity.java
@@ -11,17 +11,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.sample.unimedical.R;
 
 public class TutorialActivity extends AppCompatActivity {
-    Button btnLecture01;
-    Button btnLecture02;
-    Button btnLecture03;
-    Button btnLecture04;
-    Button btnLecture05;
-    Button btnLecture06;
-    Button btnLecture07;
-    Button btnLecture08;
-    Button btnLecture09;
-    Button btnLecture10;
-    Button btnLecture11;
 
     private String LECTURE_URL = "";
 
@@ -30,17 +19,17 @@ public class TutorialActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tutorial);
 
-        btnLecture01 = findViewById(R.id.btn_lec01);
-        btnLecture02 = findViewById(R.id.btn_lec02);
-        btnLecture03 = findViewById(R.id.btn_lec03);
-        btnLecture04 = findViewById(R.id.btn_lec04);
-        btnLecture05 = findViewById(R.id.btn_lec05);
-        btnLecture06 = findViewById(R.id.btn_lec06);
-        btnLecture07 = findViewById(R.id.btn_lec07);
-        btnLecture08 = findViewById(R.id.btn_lec08);
-        btnLecture09 = findViewById(R.id.btn_lec09);
-        btnLecture10 = findViewById(R.id.btn_lec10);
-        btnLecture11 = findViewById(R.id.btn_lec11);
+        Button btnLecture01 = findViewById(R.id.btn_lec01);
+        Button btnLecture02 = findViewById(R.id.btn_lec02);
+        Button btnLecture03 = findViewById(R.id.btn_lec03);
+        Button btnLecture04 = findViewById(R.id.btn_lec04);
+        Button btnLecture05 = findViewById(R.id.btn_lec05);
+        Button btnLecture06 = findViewById(R.id.btn_lec06);
+        Button btnLecture07 = findViewById(R.id.btn_lec07);
+        Button btnLecture08 = findViewById(R.id.btn_lec08);
+        Button btnLecture09 = findViewById(R.id.btn_lec09);
+        Button btnLecture10 = findViewById(R.id.btn_lec10);
+        Button btnLecture11 = findViewById(R.id.btn_lec11);
 
         btnLecture01.setOnClickListener(onClickListener);
         btnLecture02.setOnClickListener(onClickListener);
@@ -57,7 +46,7 @@ public class TutorialActivity extends AppCompatActivity {
     }
 
 
-    private View.OnClickListener onClickListener = v -> {
+    private final View.OnClickListener onClickListener = v -> {
         switch (v.getId()) {
             case R.id.btn_lec01:
                 LECTURE_URL = "https://m.blog.naver.com/unimedical777/222442112053";
