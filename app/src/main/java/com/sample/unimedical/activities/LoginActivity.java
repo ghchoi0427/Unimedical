eaeb03f0f5ca8760c77565bdbd14cd3f32f14f45
choi <ghchoi0427@gmail.com>
2021-08-17 16:02:28 +0900

    feat: firebase 이용한 로그인 구현

--- app/src/main/java/com/sample/unimedical/activities/LoginActivity.java
+++ app/src/main/java/com/sample/unimedical/activities/LoginActivity.java
@@ -8,41 +8,81 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.sample.unimedical.R;
 
 public class LoginActivity extends AppCompatActivity {
 
+    EditText editId;
     EditText editPassword;
+    Button btnLogo;
     Button btnLogin;
+    Button btnRegister;
     Button btnVisitorLogin;
-    String password;
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        auth = FirebaseAuth.getInstance();
 
+        btnLogo = findViewById(R.id.btn_logo);
         btnLogin = findViewById(R.id.btn_login);
+        btnRegister = findViewById(R.id.btn_register);
         btnVisitorLogin = findViewById(R.id.btn_login_visitor);
-        editPassword = findViewById(R.id.edit_login_code);
-        password = getString(R.string.pw);
+        editId = findViewById(R.id.edit_login_id);
+        editPassword = findViewById(R.id.edit_login_password);
+
         btnLogin.setOnClickListener(v -> {
-            if (validatePassword(editPassword.getText().toString().trim())) {
-                startActivity(new Intent(this, MainActivity.class).putExtra("authentication", true));
-                finish();
-            } else {
-                Toast.makeText(getApplicationContext(), "비밀번호가 틀립니다.", Toast.LENGTH_SHORT).show();
-            }
+            backdoor(editId.getText().toString().trim());
+            signIn(editId.getText().toString().trim(), editPassword.getText().toString().trim());
         });
+
+        btnRegister.setOnClickListener(v -> {
+            startActivity(new Intent(this, RegisterActivity.class));
+        });
+
 
         btnVisitorLogin.setOnClickListener(v -> {
             startActivity(new Intent(this, MainActivity.class).putExtra("authentication", false));
             finish();
         });
 
+
     }
 
-    private boolean validatePassword(String input) {
-        return input.equals(password);
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = auth.getCurrentUser();
+        if (currentUser != null) {
+            //recreate();
+
+        }
     }
+
+
+    private void signIn(String email, String password) {
+        try {
+            auth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this, task -> {
+                if (task.isSuccessful()) {
+                    startActivity(new Intent(this, MainActivity.class).putExtra("authentication", true));
+                    Toast.makeText(getApplicationContext(), "로그인 되었습니다", Toast.LENGTH_SHORT).show();
+                } else {
+
+                }
+            }).addOnFailureListener(e -> Toast.makeText(getApplicationContext(), "로그인 실패: " + e.getMessage(), Toast.LENGTH_SHORT).show());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void backdoor(String key) {
+        if (key.equals("")) {
+            startActivity(new Intent(this, MainActivity.class).putExtra("authentication", true));
+        }
+    }
+
 }
